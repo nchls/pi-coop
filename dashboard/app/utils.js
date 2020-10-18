da4e22e66185ff1eca8b14df46662dfe89a607c0
Ian Nicholas <ianmnicholas@gmail.com>
2020-10-18 16:54:15 -0400

    Tab visibility change logic

--- dashboard/app/utils.js
+++ dashboard/app/utils.js
@@ -4,8 +4,15 @@ import { useRecoilState } from 'recoil';
 
 export const useAPIPoll = (atom, endpoint, pollInterval) => {
 	const [data, setter] = useRecoilState(atom);
+	let lastUpdate = 0;
+
 	useEffect(() => {
+		let interval;
+		let timeout;
+		
+		// Hit the API and populate the atom with the result
 		const update = () => {
+			lastUpdate = Date.now();
 			window.fetch(endpoint)
 				.then((response) => {
 					if (response.status > 399) {
@@ -30,10 +37,29 @@ export const useAPIPoll = (atom, endpoint, pollInterval) => {
 					});
 				});
 		};
+
+		// Set up the initial poll interval
 		update();
-		const interval = setInterval(update, pollInterval);
+		interval = setInterval(update, pollInterval);
+
+		// When the tab loses or gains focus, clear the polling
+		document.addEventListener('visibilitychange', () => {
+			clearInterval(interval);
+			clearTimeout(timeout);
+			// And if it has gained focus, reset the polling based on the last update
+			if (!document.hidden) {
+				const timeToNextUpdate = pollInterval - (Date.now() - lastUpdate);
+				timeout = setTimeout(() => {
+					update();
+					interval = setInterval(update, pollInterval);
+				}, timeToNextUpdate);
+			}
+		});
+
+		// On unmount, clear all intervals
 		return () => {
 			clearInterval(interval);
+			clearTimeout(timeout);
 		};
 	}, []);
 };
